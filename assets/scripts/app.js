66e10ae225a82c4e431076059b71b5d200319169
MassouAnas <massouanas3@gmail.com>
2022-04-13 07:14:11 +0000

    adding logging sorta

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,8 @@
 const defautlvalue = 0;
 let currentvalue = defautlvalue;
+//adding logging to our app 
+let logEntries = [];
+
 
 function getUserNum() {
   return parseInt(userInput.value);
@@ -10,29 +13,49 @@ function theFinalOutput(operator, resultBeforeCalc, calcNumber) {
  outputResult(currentvalue, calcDescription)
 }
 
+function LogData(TypeOperation, TheFirstValue, enteredNumber, currentResult) {
+  const entryLog = {
+    operation: TypeOperation,
+    previousresult: TheFirstValue,
+    number: enteredNumber,
+    result: currentResult,     
+};
+logEntries.push(entryLog);
+console.log(logEntries);
+
+}
+
+
 function add() {
   const numberProvided = getUserNum();
   const initialvalue = currentvalue;
   currentvalue = currentvalue + numberProvided;
   theFinalOutput('+', initialvalue, numberProvided)
+  LogData('Addition', initialvalue, numberProvided, currentvalue)
   
 }
 function substract() {
   const numberProvided = getUserNum();
   const initialvalue = currentvalue;
   theFinalOutput('-', initialvalue, numberProvided)
+  LogData('Substraction', initialvalue, numberProvided, currentvalue)
+
 }
 function multiply() {
   const numberProvided = getUserNum();
   const initialvalue = currentvalue;
   currentvalue = currentvalue * numberProvided;
   theFinalOutput('*', initialvalue, numberProvided)
+  LogData('Multiplication', initialvalue, numberProvided, currentvalue)
+
 }
 function divide() {
   const numberProvided = getUserNum();
   const initialvalue = currentvalue;
   currentvalue = currentvalue / numberProvided;
   theFinalOutput('/', initialvalue, numberProvided)
+  LogData('Division', initialvalue, numberProvided, currentvalue)
+
 }
 
 addBtn.addEventListener("click", add);
